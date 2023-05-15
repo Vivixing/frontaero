333a6d329284f0b8371ad81e0364e2648ae8c3c0
Vivixing <echeverri.vivian1120@gmail.com>
2023-05-15 17:03:35 -0500

    Form crear Aeropuerto corregido

--- src/app/administrador/crear-avion/crear-avion.component.ts
+++ src/app/administrador/crear-avion/crear-avion.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-avion',
   templateUrl: './crear-avion.component.html',
   styleUrls: ['./crear-avion.component.css']
 })
-export class CrearAvionComponent {
+export class CrearAvionComponent implements OnInit{
+constructor(){}
+
+  ngOnInit(): void {
+  }
 
 }
