aaa2f296a9a1b9903777a29cd7267cd66457a186
Vivixing <echeverri.vivian1120@gmail.com>
2023-05-15 19:46:15 -0500

    form avión creado junto con su ts

--- src/app/administrador/crear-avion/crear-avion.component.ts
+++ src/app/administrador/crear-avion/crear-avion.component.ts
@@ -1,5 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { Avion } from 'src/app/interfaces/avion';
+import { AvionService } from 'src/app/services/avion.service';
 
 @Component({
   selector: 'app-crear-avion',
@@ -7,9 +9,45 @@ import { FormBuilder,FormGroup,Validators } from '@angular/forms';
   styleUrls: ['./crear-avion.component.css']
 })
 export class CrearAvionComponent implements OnInit{
-constructor(){}
+
+  constructor(private avionService:AvionService,
+              private fb:FormBuilder){}
 
   ngOnInit(): void {
+  }
+
+  avionFormulario: FormGroup = this.fb.group({
+    modelo:['',[Validators.maxLength(30),Validators.required],Validators.pattern(/^[a-zA-Z]{4,}[0-9]{3,}[\/\-]?[a-zA-Z0-9\/\-]*$/)]
+  })
+
+  crearAvion():void{
+    const datos = this.avionFormulario.value
+
+    const avion : Avion ={
+      modelo: datos.modelo,
+      estado: 'Activo'
+    };
+    console.log(avion);
+
+    this.avionService.crearAvion(avion)?.subscribe(data=>{
+      if(data==null){
+        console.log('No se puede crear el avión');
+        return;
+      }else{
+        console.log('avión creado con éxito');
+        this.avionFormulario.reset()
+      }
+    },
+    err=>{
+      let errorMensaje = 'Error al crear el avión';
+
+      if(err.error && err.error.mensaje){
+        errorMensaje = err.error.mensaje;
+      }
+
+      console.log(errorMensaje);
+    })
+
   }
 
 }
