5ba0edb2e55f0e33ea14261d76eb5eb3fc69b910
Vivixing <echeverri.vivian1120@gmail.com>
2023-05-17 18:44:01 -0500

    Añadido en formualrio de crear aeropuerto el select de país

--- src/app/administrador/crear-aeropuerto/crear-aeropuerto.component.ts
+++ src/app/administrador/crear-aeropuerto/crear-aeropuerto.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Aeropuerto } from 'src/app/interfaces/aeropuerto';
 import { AeropuertosService } from 'src/app/services/aeropuertos.service';
+import { LocacionService } from 'src/app/services/locacion.service';
+import { Pais } from 'src/app/interfaces/pais';
 
 
 @Component({
@@ -12,22 +14,50 @@ import { AeropuertosService } from 'src/app/services/aeropuertos.service';
 })
 export class CrearAeropuertoComponent implements OnInit {
 
+  paises: any[] = [];
+  /*ciudades: any[] = [];*/
+
   IataenUso: boolean = false
 
   constructor(private aeropuertosService: AeropuertosService,
+    private locationService: LocacionService,
     private router: Router,
     private fb: FormBuilder) { }
 
   ngOnInit(): void {
-
+    this.loadCountries()
+    
   }
 
   aeropuertoformulario: FormGroup = this.fb.group({
     //id :['',Validators.required],
     nombre: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]{6,30}?[A-Za-z-]+$/)]],
-    iata: ['', [Validators.required,Validators.minLength(3), Validators.maxLength(3) ,Validators.pattern(/^[A-Za-z]+$/)]],
+    pais: ['', Validators.required],
+    ciudad: ['', Validators.required],
+    iata: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3), Validators.pattern(/^[A-Za-z]+$/)]],
     ubicacion: ['', Validators.required]
   })
+
+  loadCountries() {
+    this.locationService.obtenerPaises().subscribe((data:any[]) => {
+      this.paises = data;
+    },
+      error => {
+        console.log('error al obtener los paises',error);
+      });
+  }
+  /*
+  buscarCiudades(pais: string) {
+    this.locationService.obtenerCiudades(pais).subscribe(
+      (data) => {
+        this.ciudades = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  */
 
   crearAeropuerto(): void {
     const datos = this.aeropuertoformulario.value
@@ -35,10 +65,11 @@ export class CrearAeropuertoComponent implements OnInit {
     const aeropuerto: Aeropuerto = {
       nombre: datos.nombre,
       iata: datos.iata,
-      ubicacion: datos.ubicacion,
+      ubicacion: `${datos.pais} - ${datos.ciudad}`,
       estado: 'Activo'
     };
     console.log(aeropuerto);
+
     let listadoAeropuertos: Aeropuerto[] = []
     this.aeropuertosService.obtenerAeropuertos().subscribe(res => {
       listadoAeropuertos = res
---
+++ src/app/services/locacion.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pais } from '../interfaces/pais';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocacionService {
+  
+  constructor(private http: HttpClient) { }
+
+  urlApiLocacion = 'https://restcountries.com/v2/all';
+
+  obtenerPaises():Observable<any[]> {
+    return this.http.get<any[]>(`${this.urlApiLocacion}`);
+  }
+
+  /*obtenerCiudades(pais: string): Observable<any> {
+    return this.http.get<any>(`${this.urlApiLocacion}//name/${pais}?fullText=true`);
+  }*/
+
+  
+}
