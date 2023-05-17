2482414c2035d3dfadd4f892ff194634db7e8ae7
Vivixing <echeverri.vivian1120@gmail.com>
2023-05-16 22:04:55 -0500

    Actualizado crearVuelo, dashboard y login

--- src/app/administrador/crear-vuelo/crear-vuelo.component.ts
+++ src/app/administrador/crear-vuelo/crear-vuelo.component.ts
@@ -1,11 +1,22 @@
 import { Component,OnInit } from '@angular/core';
 import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { VueloService } from 'src/app/services/vuelo.service';
 
 @Component({
   selector: 'app-crear-vuelo',
   templateUrl: './crear-vuelo.component.html',
   styleUrls: ['./crear-vuelo.component.css']
 })
-export class CrearVueloComponent {
+export class CrearVueloComponent implements OnInit {
 
+  constructor(private vueloService:VueloService,
+              private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  vueloFormulario: FormGroup = this.fb.group({
+    
+  })
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,36 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  credencialesNoValidas:boolean = false
+
+  usuario ={
+    cedula: "",
+    email: ""
+  }
+
+  constructor(private usuarioService:UsuarioService,
+              private route:Router,
+              private fb : FormBuilder){}
+
+  loginFormulario: FormGroup = this.fb.group({
+    cedula: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(10),Validators.pattern(/^[0-9]{8,10}$/)]],
+    email:['',[Validators.required,Validators.pattern(/[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}/)]]
+  })
+
+  iniciarSesion(){
+    this.usuario.cedula = this.loginFormulario.value['cedula']
+    this.usuario.email =this.loginFormulario.value['email']
+    //Crear en el back obtener usuario por Cédula o implementar el del obtener usuario por Id
+
+  }
+
+  usuarioExistenteEnBd(usuarioExite: Usuario){
+    if(usuarioExite.rolUsuario_rousid == 1){ //el id 1 de rolUsuario es el Admin
+      if(usuarioExite.correo == this.usuario.email){
+        this.route.navigate(["administrador"])
+        this.credencialesNoValidas = false
+      }
+      this.credencialesNoValidas = true
+    }
+  }
 }
