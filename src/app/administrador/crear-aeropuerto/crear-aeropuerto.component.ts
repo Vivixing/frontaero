8fc2603fb12dc9e54d94e2af7952e8a9dcaf826c
Vivixing <echeverri.vivian1120@gmail.com>
2023-05-17 16:35:18 -0500

    Update Crear aeropuerto formulario

--- src/app/administrador/crear-aeropuerto/crear-aeropuerto.component.ts
+++ src/app/administrador/crear-aeropuerto/crear-aeropuerto.component.ts
@@ -24,8 +24,8 @@ export class CrearAeropuertoComponent implements OnInit {
 
   aeropuertoformulario: FormGroup = this.fb.group({
     //id :['',Validators.required],
-    nombre: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(5), Validators.pattern(/^[a-zA-Z]{6,}?[A-Za-z-]+$/)]],
-    iata: ['', [Validators.required, Validators.maxLength(3), Validators.minLength(3), Validators.pattern(/^[A-Za-z]+$/)]],
+    nombre: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]{6,30}?[A-Za-z-]+$/)]],
+    iata: ['', [Validators.required,Validators.minLength(3), Validators.maxLength(3) ,Validators.pattern(/^[A-Za-z]+$/)]],
     ubicacion: ['', Validators.required]
   })
 
