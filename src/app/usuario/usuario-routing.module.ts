b7b5062eef1119b2650409b025229a4600d88ee6
valentina <valeagu1710@gmail.com>
2023-06-04 17:11:23 -0500

    Hecho componente factura

--- src/app/usuario/usuario-routing.module.ts
+++ src/app/usuario/usuario-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexPageComponent } from './index-page/index-page.component';
-
+import {ReservaUsuarioComponent} from './reserva-usuario/reserva-usuario.component'
+import {PagoUsuarioComponent} from './pago-usuario/pago-usuario.component'
+import {FacturaUsuarioComponent} from './factura-usuario/factura-usuario.component'
 
 
 const routes: Routes = [
-  {path:'indexUsuario',component:IndexPageComponent}
-
+  {path:'indexUsuario',component:IndexPageComponent},
+  {path:'facturaUsuario',component:FacturaUsuarioComponent},
+  {path:'reservaUsuario',component:ReservaUsuarioComponent},
+  {path:'pagoUsuario',component:PagoUsuarioComponent}
 ];
 
 @NgModule({
--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioRoutingModule } from './usuario-routing.module';
 import { IndexPageComponent } from './index-page/index-page.component';
-import { FacturaUsuarioComponent } from './factura-usuario/factura-usuario.component';
-import { PagoUsuarioComponent } from './pago-usuario/pago-usuario.component';
-import { ReservaUsuarioComponent } from './reserva-usuario/reserva-usuario.component';
-
+import {ReservaUsuarioComponent} from './reserva-usuario/reserva-usuario.component'
+import {PagoUsuarioComponent} from './pago-usuario/pago-usuario.component'
+import {FacturaUsuarioComponent} from './factura-usuario/factura-usuario.component'
 
 @NgModule({
   declarations: [
     IndexPageComponent,
-    FacturaUsuarioComponent,
+    ReservaUsuarioComponent,
     PagoUsuarioComponent,
-    ReservaUsuarioComponent
+    FacturaUsuarioComponent
   ],
   imports: [
     CommonModule,
