70c4e7821d297da7e5f7b1d4396d6fed3b1a2f28
Vivixing <echeverri.vivian1120@gmail.com>
2023-06-05 15:28:19 -0500

    Arreglado Index Usuario con el form del vuelo

--- src/app/usuario/index-page/index-page.component.ts
+++ src/app/usuario/index-page/index-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Input } from '@angular/core';
+import { VueloModelo } from 'src/app/interfaces/vuelo';
+import { Aeropuerto } from 'src/app/interfaces/aeropuerto';
+import { AeropuertosService } from 'src/app/services/aeropuertos.service';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-index-page',
   templateUrl: './index-page.component.html',
   styleUrls: ['./index-page.component.css']
 })
-export class IndexPageComponent {
+export class IndexPageComponent implements OnInit{
+
+  @Input() listadoVuelos !:VueloModelo[];
+  aeropuertos:Aeropuerto[]=[];
+
+  constructor(private aeropuertoService:AeropuertosService,private fb:FormBuilder){}
+  
+  ngOnInit(): void {
+    this.aeropuertoService.obtenerAeropuertos().subscribe( response =>{
+      this.aeropuertos = response
+      this.aeropuertos = this.aeropuertos.filter(aeropuerto=> aeropuerto.estado === 'Activo' || aeropuerto.estado ==='Activa')
+    })
+  }
+
+  viajeForm: FormGroup = this.fb.group({
+    viajeOrigen:[Validators.required],
+    viajeDestino:[Validators.required]
+  })
 
 }
