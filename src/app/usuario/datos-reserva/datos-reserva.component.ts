f9e0534998ee505ea409dfd17f94a45495f305d8
Vivixing <echeverri.vivian1120@gmail.com>
2023-06-06 18:20:15 -0500

    Completada lógica del componente datos reserva para el usuario

--- src/app/usuario/datos-reserva/datos-reserva.component.ts
+++ src/app/usuario/datos-reserva/datos-reserva.component.ts
@@ -19,7 +19,57 @@ export class DatosReservaComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    const idReserva = parseInt(this.router.url.split("/").pop() || "0");
+    this.reservaService.obtenerDatosReserva(idReserva);
+    this.reservaService.obtenerReservaUsuarioModelo().subscribe(response => {
+      this.datosReserva = response
+    })
   }
 
+  pagarReserva() {
+    this.datosReserva.reservaDatos.estado = 'Activo'
+    this.datosReserva.reservaDatos.estadoPago = 'Activo'
+    console.log(this.datosReserva)
+    this.reservaService.actualizarReserva(this.datosReserva.reservaDatos).subscribe(reserva => {
+      this.datosReserva.reservaDatos = reserva
+      console.log(reserva)
+      this.router.navigate([])
+    })
+  }
+
+  cancelarReserva() {
+    this.datosReserva.reservaDatos.estado = 'Inactivo'
+    this.datosReserva.reservaDatos.estadoPago = 'Inactivo'
+    console.log(this.datosReserva)
+    this.reservaService.actualizarReserva(this.datosReserva.reservaDatos).subscribe(reserva => {
+      this.datosReserva.reservaDatos = reserva
+      console.log(this.datosReserva.reservaDatos)
+      this.router.navigate([])
+    })
+  }
+  organizacionAeropuertos() {
+    const escalas = this.datosReserva.escalasDatos
+
+    escalas.sort((a, b) => {
+      const fechaHoraLlegada1 = new Date(a.horaLlegada).getTime();
+      const fechaHoraLlegada2 = new Date(b.horaLlegada).getTime();
+
+      if (fechaHoraLlegada1 < fechaHoraLlegada2) {
+        return -1;
+      } else if (fechaHoraLlegada1 < fechaHoraLlegada2) {
+        return 1;
+      } else {
+        return 0
+      }
+    });
+
+    for (let i = 0; i < escalas.length; i++) {
+      let aeropuertos = []
+      for(let j=0; j<this.datosReserva.aeropuertosDatos.length; j++){
+        if(escalas[i].aereoIdOrigen == this.datosReserva.aeropuertosDatos[j].aeroId)aeropuertos[0]=(this.datosReserva.aeropuertosDatos[j])
+        if(escalas[i].aereoIdDestino == this.datosReserva.aeropuertosDatos[j].aeroId)aeropuertos[1]=(this.datosReserva.aeropuertosDatos[j])
+      }
+      this.listadoAeropuertos.push(aeropuertos)
+    }
+  }
 }
